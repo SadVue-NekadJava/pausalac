5abdc1b7f6aa4f684b8f9e7649f1745bfab84b6c
lazarbajic <lazarbajic@hotmail.com>
2018-11-05 13:05:58 +0100

    Moja firma

--- pages/MojaFirma.vue
+++ pages/MojaFirma.vue
@@ -1,11 +1,171 @@
-<template lang="html">
+<template >
+<v-container >
+  <v-layout row wrap>
+    <v-flex class="forma pa-5"  md6 offset-md3>
+<h1 class="text-xs-center">{{imeFirme}}</h1>
+<hr>
+<v-layout row wrap>
+  <v-flex>
+    <v-layout class="pt-4" row wrap>
+<v-flex  xs7>
+<h3 >Pun naziv:</h3>
+</v-flex>
+<v-flex hidden-sm-and-up xs5>
+</v-flex>
+<v-flex xs5>
+  <h3 class="podaci">{{punNaziv}}</h3>
+</v-flex>
+    </v-layout>
 
+    <v-layout class="pt-4" row wrap>
+<v-flex  xs7>
+<h3 >Maticni broj:</h3>
+</v-flex>
+<v-flex hidden-sm-and-up xs5>
+</v-flex>
+<v-flex xs5>
+  <h3 class="podaci">{{maticniBroj}}</h3>
+</v-flex>
+    </v-layout>
+    <v-layout class="pt-4" row wrap>
+<v-flex  xs7>
+<h3 >Pib:</h3>
+</v-flex>
+<v-flex hidden-sm-and-up xs5>
+</v-flex>
+<v-flex xs5>
+  <h3 class="podaci">{{pib}}</h3>
+</v-flex>
+    </v-layout>
+    <v-layout class="pt-4" row wrap>
+<v-flex  xs7>
+<h3 >Ziro racun:</h3>
+</v-flex>
+<v-flex hidden-sm-and-up xs5>
+</v-flex>
+<v-flex xs5>
+  <h3 class="podaci">{{ziroRacun}}</h3>
+</v-flex>
+    </v-layout>
+    <v-layout class="pt-4" row wrap>
+<v-flex  xs7>
+<h3 >Sifra delatnosti:</h3>
+</v-flex>
+<v-flex hidden-sm-and-up xs5>
+</v-flex>
+<v-flex xs5>
+  <h3 class="podaci">{{sifraDelatnosti}}</h3>
+</v-flex>
+    </v-layout>
+    <v-layout class="pt-4" row wrap>
+  <v-flex  xs7>
+  <h3 >Opis delatnosti:</h3>
+  </v-flex>
+  <v-flex hidden-sm-and-up xs5>
+  </v-flex>
+  <v-flex xs5>
+  <h3 class="podaci">{{opisDelatnosti}}</h3>
+  </v-flex>
+    </v-layout>
+<h3 class="pt-5 pb-1 blue-grey--text"><em>Kontakt podaci</em></h3>
+<hr>
+<v-layout class="pt-3" row wrap>
+  <v-flex xs7>
+    <h3 >Adresa:</h3>
+
+  </v-flex>
+  <v-flex hidden-sm-and-up xs5>
+  </v-flex>
+  <v-flex xs5>
+<h3 class="podaci">{{adresa}}</h3>
+  </v-flex>
+</v-layout>
+
+<v-layout class="pt-3" row wrap>
+  <v-flex xs7>
+    <h3 >Mesto:</h3>
+
+  </v-flex>
+  <v-flex hidden-sm-and-up xs5>
+  </v-flex>
+  <v-flex xs5>
+<h3 class="podaci">{{opstina}}, {{grad}}</h3>
+  </v-flex>
+</v-layout>
+<v-layout class="pt-3" row wrap>
+  <v-flex xs7>
+    <h3 >Telefon:</h3>
+
+  </v-flex>
+  <v-flex hidden-sm-and-up xs5>
+  </v-flex>
+  <v-flex xs5>
+<h3 class="podaci">{{telefon}}</h3>
+  </v-flex>
+</v-layout>
+<v-layout class="pt-3"  row wrap>
+  <v-flex  xs7>
+    <h3 >Email:</h3>
+
+  </v-flex>
+  <v-flex hidden-sm-and-up xs5>
+  </v-flex>
+  <v-flex xs5>
+<h3 class="podaci">{{email}}</h3>
+  </v-flex>
+</v-layout>
+  </v-flex>
+</v-layout>
+    </v-flex>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      imeFirme:'',
+      punNaziv:'',
+      maticniBroj:'',
+      pib:'',
+      sifraDelatnosti:'',
+      opisDelatnosti:'',
+      ziroRacun:'',
+      adresa:'',
+      opstina:'',
+      grad:'',
+      telefon:'',
+      email:''
+    }
+  },
+  mounted(){
+    axios.get("http://837s121.mars-e1.mars-hosting.com/getCompany",{
+                  params:{ sid: localStorage.getItem('sessionid')  }
+                 }).then(response => {
+console.log(response.data.res[0]);
+this.imeFirme=response.data.res[0].fir_naziv;
+this.punNaziv=response.data.res[0].fir_pun_naziv;
+this.maticniBroj=response.data.res[0].fir_maticni_broj;
+this.pib=response.data.res[0].fir_pib;
+this.sifraDelatnosti=response.data.res[0].fir_sifra_delatnosti;
+this.opisDelatnosti=response.data.res[0].fir_opis_delatnosti;
+this.ziroRacun=response.data.res[0].fir_ziro_racun;
+this.adresa=response.data.res[0].fir_adresa;
+this.opstina=response.data.res[0].ops_naziv ;
+this.grad=response.data.res[0].gra_naziv;
+this.telefon=response.data.res[0].fir_telefon;
+this.email=response.data.res[0].fir_mejl;
+                 });
+
+  }
 }
+
 </script>
 
-<style lang="css">
+<style >
+.podaci{
+  color:#68645a;
+  font-weight: 400;
+}
 </style>
